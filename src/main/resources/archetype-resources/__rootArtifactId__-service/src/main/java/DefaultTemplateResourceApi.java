c96689288213b5fa524caafd8ff742f03326d01a
Vasily-Ermakov <rec0rd.alterego@gmail.com>
2020-03-25 15:39:34 +0300

    Group resources by tags

--- src/main/resources/archetype-resources/__rootArtifactId__-service/src/main/java/DefaultTemplateResourceApi.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-service/src/main/java/DefaultTemplateResourceApi.java
@@ -2,13 +2,13 @@ package $package;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import p.api.TemplateApi;
-import p.model.Template;
+import ${package}.api.TemplateResourceApi;
+import ${package}.model.Template;
 
 import java.util.Optional;
 
 @Controller
-public class DefaultTemplateApi implements TemplateApi {
+public class DefaultTemplateResourceApi implements TemplateResourceApi {
 
   @Override
   public ResponseEntity<Template> getTemplate() {
