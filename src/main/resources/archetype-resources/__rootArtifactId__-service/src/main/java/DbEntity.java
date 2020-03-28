7befd8f96df0d3c1e8f94f40a37816ffad93d0be
Alex Apon <nemanwir@gmail.com>
2020-03-28 19:29:18 +0300

    Lombok dependency

--- src/main/resources/archetype-resources/__rootArtifactId__-service/src/main/java/DbEntity.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-service/src/main/java/DbEntity.java
@@ -1,28 +1,17 @@
 package $package;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
 public class DbEntity {
 
     @Id
     private Long id;
     private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
